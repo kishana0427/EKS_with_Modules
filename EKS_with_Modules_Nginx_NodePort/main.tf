7e586b6c197526ea4e017d1bc5fcab880f2a16c9
kishana0427 <95524882+kishana0427@users.noreply.github.com>
2025-09-11 12:44:53 +0530

    Add files via upload

---
+++ EKS_with_Modules_Nginx_NodePort/main.tf
@@ -0,0 +1,187 @@
+############################################################
+# Providers
+############################################################
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.9"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+############################################################
+# VPC Module
+############################################################
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway     = true
+  enable_vpn_gateway     = false
+  map_public_ip_on_launch = true
+
+  public_subnet_tags = {
+    Name = "my-cluster-public"
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+
+  private_subnet_tags = {
+    Name = "my-cluster-private"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+
+}
+
+
+############################################################
+# Security Group for Nodes
+############################################################
+resource "aws_security_group" "eks_nodes" {
+  vpc_id = module.vpc.vpc_id
+  name   = "eks-nodes-sg"
+
+  ingress {
+    description = "Allow all node-to-node traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow 30080"
+    from_port   = 30080
+    to_port     = 30080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+
+############################################################
+# EKS Module
+############################################################
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = var.cluster_name
+  kubernetes_version = "1.33"
+
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = { before_compute = true }
+    kube-proxy             = {}
+    vpc-cni                = { before_compute = true }
+  }
+
+  endpoint_public_access                 = true
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.public_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 2
+      min_size     = 1
+      max_size     = 3
+
+      instance_types = ["t3.medium"]
+      vpc_security_group_ids = [aws_security_group.eks_nodes.id]
+      iam_role_arn = aws_iam_role.eks_node_role.arn
+
+      labels = {
+        role = "general"
+      }
+
+      tags = {
+        Name        = "eks-default-nodegroup"
+        Environment = "dev"
+        Terraform   = "true"
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
---
+++ EKS_with_Modules_Nginx_NodePort/outputs.tf
@@ -0,0 +1,23 @@
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
+}
+
+output "node_group_sg" {
+  value = aws_security_group.eks_nodes.id
+}
