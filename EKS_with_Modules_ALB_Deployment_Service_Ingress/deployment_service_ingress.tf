779fe0a5c9bb25c301b8a214d7bf588b127812f2
kishana0427 <95524882+kishana0427@users.noreply.github.com>
2025-09-30 11:14:55 +0530

    Add files via upload

---
+++ EKS_with_Modules_ALB_Deployment_Service_Ingress/deployment_service_ingress.tf
@@ -0,0 +1,93 @@
+############################################################
+# Kubernetes App (Deployment + NodePort Service)
+############################################################
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:latest"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+
+#Option A (Recommended) → ClusterIP + Target Type = IP
+#########
+#Cleaner, avoids opening NodePorts on worker nodes.
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx"
+  }
+
+  spec {
+    selector = {
+      app = "nginx"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "ClusterIP"
+  }
+}
+
+resource "kubernetes_ingress_v1" "nginx_ingress" {
+  metadata {
+    name = "nginx-ingress"
+    annotations = {
+      "kubernetes.io/ingress.class"           = "alb"
+      "alb.ingress.kubernetes.io/scheme"      = "internet-facing"
+      "alb.ingress.kubernetes.io/target-type" = "ip"
+    }
+  }
+
+  spec {
+    rule {
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.nginx.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
